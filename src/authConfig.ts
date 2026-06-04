45069a5d85b6a0e069a52486dd51b191f88178cc
Muliki Varalakshmi <vmuliki@AD.MSYSTECHNOLOGIES.COM>
2026-06-04 13:04:19 +0530

    auth config

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,6 +1,6 @@
 export const msalConfig = {
   auth: {
-    clientId: "YOUR_CLIENT_ID",
+    clientId: "2abc845c-15fc-414f-b319-e7da37e9b563",
     authority:
       "https://login.microsoftonline.com/0540b69a-7ff8-497e-a99c-045243c2b269",
     redirectUri: window.location.origin,
