ad3fdb2447abcade07f2acb2f2da075dc4e6fbcd
Muliki Varalakshmi <vmuliki@AD.MSYSTECHNOLOGIES.COM>
2026-06-11 18:28:16 +0530

    testing graph api

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,20 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { MsalProvider } from "@azure/msal-react";
 import { msalInstance } from "./auth/authConfig";
 
 async function bootstrap() {
-  console.log("HASH:", window.location.hash);
-
   await msalInstance.initialize();
 
-  if (window.location.hash.includes("code=")) {
-    console.log("PROCESSING AUTH CALLBACK");
-
-    const response =
-      await msalInstance.handleRedirectPromise();
-
-    console.log("AUTH RESPONSE", response);
-
-    return; // TEMPORARY
-  }
+  await msalInstance.handleRedirectPromise();
 
   ReactDOM.createRoot(
     document.getElementById("root")!
-  ).render(<App />);
+  ).render(
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
+  );
 }
 
 bootstrap();
