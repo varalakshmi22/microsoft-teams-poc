a3281e1a74cf153bdd41e55e71c5b72def76831c
Muliki Varalakshmi <vmuliki@AD.MSYSTECHNOLOGIES.COM>
2026-06-12 12:07:37 +0530

    testing graph api

--- src/auth/authConfig.ts
+++ src/auth/authConfig.ts
@@ -5,7 +5,7 @@ export const msalInstance = new PublicClientApplication({
     clientId: "2abc845c-15fc-414f-b319-e7da37e9b563",
     authority:
       "https://login.microsoftonline.com/0540b69a-7ff8-497e-a99c-045243c2b269",
-    redirectUri: "https://microsoft-teams-poc.vercel.app",
+    redirectUri: "https://microsoft-teams-poc.vercel.app/",
 
   },
   cache: {
